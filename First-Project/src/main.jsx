e635e38951e25d076a4ab771f35316a4dc6a3ebb
lakshay122007 <lakshay11goyal@gmail.com>
2026-03-23 09:17:47 +0530

    vite@react added

--- First-Project/src/main.jsx
+++ First-Project/src/main.jsx
@@ -1,11 +1,10 @@
-
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-let section = "Section-A"
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <div>
-    <h1>Lakshay Goyal</h1>
-    <p>Hello goood morning..🌅</p>
-    <p>{section}</p>
-  </div>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
