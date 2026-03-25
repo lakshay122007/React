6d0e68b3b0e0fdc945b53a73f5278a8cf5db29b9
lakshay122007 <lakshay11goyal@gmail.com>
2026-03-25 19:17:00 +0530

    Lec-3

--- First-Project/src/App.jsx
+++ First-Project/src/App.jsx
@@ -8,13 +8,19 @@ const student = {
   friend : "gulu gulu",
   meaning : "invincible"
 }
+const arr = [1,2,3,4,5]
 function App() {
 
   return (
     <>
     <p>This person named {student.name} is somene who's friend is {student.friend} is {student.invincible} yet huluhulu</p>
+    <ul>
+      {arr.forEach((ele) =>{
+        <li>{ele}</li>
+      })}
+    </ul>
   </> 
-  )
+  ) 
 }
 
 export default App
