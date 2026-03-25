9240aee59dca35197b9644e9a519a9128016d63e
lakshay122007 <lakshay11goyal@gmail.com>
2026-03-25 09:48:19 +0530

    practice

--- First-Project/src/main.jsx
+++ First-Project/src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 // import "./index.css"
 import Header from './Header.jsx'
 import Mainn from './Mainn.jsx'
-
+import App from './App.jsx'
 const c = "gulugulu"
 createRoot(document.getElementById('root')).render(
   <div>
     <Header />
     <Mainn />
+    <App />
   </div>
 
 )
