fce8930c1c9d583fd6d0869fe60d4402831fcf26
lakshay122007 <lakshay11goyal@gmail.com>
2026-04-09 10:29:29 +0530

    Input control

--- First-Project/src/components/inputComp.jsx
+++ First-Project/src/components/inputComp.jsx
@@ -2,19 +2,25 @@ import {useState} from 'react'
 
 export default function InputComp(){
     const [inputVal, setInput] = useState("")
+    const [isEmpty, setIsEm] = useState(true)
 
     function handleInput(e){
         console.log(e.target.value)
         setInput(e.target.value) //state changes, inputVal = NA, component rerenders 
+        if(e.target.value.length > 0) setIsEm(false)
     }
     return (
-        <input 
-        type='text' 
-        onChange = {handleInput} 
-        placeholder='Enter Password'
-        value = {inputVal}>
+        <div>
+            <input 
+            type='text' 
+            onChange = {handleInput} 
+            placeholder='Enter Password'
+            value = {inputVal}>
 
-        </input>
+            </input>
+            <p>{inputVal.length < 8 ? "Password is too Short" : "Valid password"}</p>
+        </div>
+
     )
 }
 
