cc5b01dff3709d2097f21279812f430f770e89be
lakshay122007 <lakshay11goyal@gmail.com>
2026-04-13 10:02:39 +0530

    useEffect working

--- First-Project/src/App_Effect.jsx
+++ First-Project/src/App_Effect.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 export default function App_Effect(){
     useEffect(()=>{
         console.log("HUIHUIHUI")
-    })
+    },[])
     return(
         <>
         <p>hello</p>
